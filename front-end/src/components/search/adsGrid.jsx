99a0ab872998f7097eb2e774b51e1564705e647f
Twinkle Vaid <twinkle@Twinkles-MacBook-Pro.local>
2022-11-11 04:22:29 +0530

    Display info

--- front-end/src/components/search/adsGrid.jsx
+++ front-end/src/components/search/adsGrid.jsx
@@ -1,4 +1,21 @@
 export const AdsGrid = ({ data }) => {
   console.log("data", data);
-  return <>Ads Grid</>;
+  return (
+    <div>
+      {data.length &&
+        data.map((ad) => {
+          return (
+            <>
+              <div>company url: {ad.company.url}</div>
+              <div>company name: {ad.company.name}</div>
+              <div>CTA: {ad.cta}</div>
+              <div>Description: {ad.description}</div>
+              <div>Headline: {ad.headline}</div>
+              <div>Image Url: {ad.imgUrl}</div>
+              <div>primary Text: {ad.primaryText}</div>
+            </>
+          );
+        })}
+    </div>
+  );
 };
